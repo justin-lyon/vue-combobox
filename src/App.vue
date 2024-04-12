17b1d35c048f86c7f5f8f3fd984f5389b7de4d76
Justin Lyon <9729366+justin-lyon@users.noreply.github.com>
2024-04-12 14:29:51 +0900

    listbox position and component border styles

--- src/App.vue
+++ src/App.vue
@@ -7,20 +7,62 @@ const options = [
     value: 1
   },
   {
+    name: 'Ivysaur',
+    value: 2
+  },
+  {
+    name: 'Venasaur',
+    value: 3
+  },
+  {
     name: 'Charmander',
     value: 4
   },
   {
+    name: 'Charmeleon',
+    value: 5
+  },
+  {
+    name: 'Charizard',
+    value: 6
+  },
+  {
     name: 'Squirtle',
     value: 7
+  },
+  {
+    name: 'Wartortle',
+    value: 8
+  },
+  {
+    name: 'Blastoise',
+    value: 9
+  },
+  {
+    name: 'Caterpie',
+    value: 10
   }
 ]
 </script>
 
 <template>
-  <main>
+  <main class="container">
     <ComboboxInput :options="options" label="Pokemon Search" is-label-hidden />
+    <div>Other Content</div>
   </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+.container {
+  margin: 0 auto;
+  margin-top: 2rem;
+  width: 50%;
+  height: 100vh;
+  /* border: 1px solid yellow; */
+}
+
+main {
+  font-size: 2rem;
+  line-height: 2.2rem;
+}
+</style>
--- src/components/ComboboxInput.vue
+++ src/components/ComboboxInput.vue
@@ -23,6 +23,7 @@ const props = defineProps({
 
 const searchTerm = ref(null)
 const isFocused = ref(false)
+const isListBoxOpen = ref(false)
 
 function onFocus() {
   isFocused.value = true
@@ -32,6 +33,35 @@ function onBlur() {
   isFocused.value = false
 }
 
+function onArrowDown() {
+  if (!isListBoxOpen.value) {
+    isListBoxOpen.value = true
+    return
+  }
+}
+
+function onArrowUp() {
+  if (!isListBoxOpen.value) {
+    isListBoxOpen.value = true
+    return
+  }
+}
+
+function onEnter() {
+  console.log('enter')
+  searchTerm.value = 'selectedOption.value'
+  isListBoxOpen.value = false
+}
+
+function onEscape() {
+  console.log('escape')
+  isListBoxOpen.value = false
+}
+
+function onChevronClicked() {
+  isListBoxOpen.value = !isListBoxOpen.value
+}
+
 const isLabelHidden = computed(() => {
   return props.labelType === 'hidden' ? 'hidden' : ''
 })
@@ -39,33 +69,47 @@ const isLabelHidden = computed(() => {
 
 <template>
   <div class="combobox">
-    <div class="form">
-      <label
-        for="comboboxInput"
-        :aria-hidden="isLabelHidden"
-        :class="{ hidden: props.isLabelHidden }"
-      >
-        <SearchIcon class="medium" />
-      </label>
-      <input
-        id="comboboxInput"
-        type="text"
-        v-model="searchTerm"
-        placeholder="🔎 Search..."
-        aria-controls="optionsList"
-        aria-autocomplete="both"
-        aria-expanded=""
-        data-active-option=""
-        aria-activedescendant=""
-        :onfocus="onFocus"
-        :onblur="onBlur"
-      />
-      <button>
-        <ChevronLeftIcon class="medium" />
-        <CloseIcon class="medium" :class="{ hidden: isFocused.value }" />
+    <div class="input-container">
+      <div class="stretch">
+        <label
+          for="comboboxInput"
+          :aria-hidden="isLabelHidden"
+          :class="{ hidden: props.isLabelHidden }"
+        >
+          <SearchIcon class="medium" />
+        </label>
+        <input
+          id="comboboxInput"
+          type="text"
+          v-model="searchTerm"
+          placeholder="🔎 Search..."
+          aria-controls="optionsList"
+          aria-autocomplete="both"
+          aria-expanded=""
+          data-active-option=""
+          aria-activedescendant=""
+          @keyup.prevent.up="onArrowUp"
+          @keyup.prevent.down="onArrowDown"
+          @keyup.prevent.enter="onEnter"
+          @keyup.prevent.esc="onEscape"
+          :onfocus="onFocus"
+          :onblur="onBlur"
+        />
+      </div>
+      <button class="" :class="{ hidden: !isListBoxOpen }">
+        <CloseIcon />
+      </button>
+      <button class="btn-chevron" :aria-expanded="isListBoxOpen" @click="onChevronClicked">
+        <ChevronLeftIcon :class="{ open: isListBoxOpen }" />
       </button>
     </div>
-    <ul id="optionsList" class="listbox" role="listbox" aria-label="listbox options">
+    <ul
+      id="optionsList"
+      class="listbox"
+      :class="{ hidden: !isListBoxOpen }"
+      role="listbox"
+      aria-label="listbox options"
+    >
       <li v-for="item in props.options" v-bind:key="item.value" role="option">
         {{ item.name }}
       </li>
@@ -74,12 +118,68 @@ const isLabelHidden = computed(() => {
 </template>
 
 <style scoped>
-.medium {
-  width: 0.8rem;
-  height: 0.8rem;
+.combobox {
+  position: relative;
+}
+
+.combobox .input-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+}
+
+.input-container .stretch {
+  flex: 1;
 }
 
 .hidden {
   display: none;
+}
+
+.input-container {
+  border: 1px solid grey;
+  border-radius: 0.25rem;
+}
+
+input {
+  border-radius: 0.25rem 0 0 0.25rem;
+}
+
+.btn-chevron {
+  border-radius: 0 0.25rem 0.25rem 0;
+}
+
+.combobox button.btn-chevron[aria-expanded='true'] svg {
+  transform: rotate(270deg) translate(0, 0);
+}
+
+input,
+ul {
+  width: 100%;
+  background-color: var(--background);
+}
+
+input,
+button,
+ul {
+  border: 0;
+  padding: 0.5rem 1rem;
+}
+
+ul {
+  opacity: 1;
+  position: absolute;
+  margin-top: 0.25rem;
+  border: 1px solid grey;
+  border-radius: 0.25rem;
+}
+
+li {
+  list-style-type: none;
+}
+
+li:hover {
+  background-color: var(--selected);
+  color: var(--font-selected-color);
 }
 </style>
